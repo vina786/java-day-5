f9ff1a9d1a5017c9a7df6522395b989eb79df4ef
vina786 <vinay76b@gmail.com>
2024-12-06 13:33:32 +0530

    Add files via upload

---
+++ countingset.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class countingset {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        while(n>0)
+        {
+            c+=n&1;
+            n>>=1;
+        }
+        System.out.println(c);
+        }
+
+    }
