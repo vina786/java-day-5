bd8403f72ee166b32c0b4cb2ed8347e989dca9d4
vina786 <vinay76b@gmail.com>
2024-12-07 05:53:36 +0530

    Add files via upload

---
+++ num1.java
@@ -0,0 +1,14 @@
+public class num1 {
+    public static void main(String args[])
+    {
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=i;j<=5;j++)
+        {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+
+        }
+    }
+}
