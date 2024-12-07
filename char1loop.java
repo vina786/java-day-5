4449f38b98159250e64526487f87428a422d1346
vina786 <vinay76b@gmail.com>
2024-12-07 05:50:14 +0530

    Add files via upload

---
+++ char1loop.java
@@ -0,0 +1,13 @@
+public class char1loop {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+            }
+    }
+}
