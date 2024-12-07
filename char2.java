ed1f14b0fa0bf195d76992e7df2622be97b5aba8
vina786 <vinay76b@gmail.com>
2024-12-07 05:50:46 +0530

    Add files via upload

---
+++ char2.java
@@ -0,0 +1,14 @@
+public class char2 {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+            }
+    }
+}
+
