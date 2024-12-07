961a446cb9c0788e41bf0d3312fb58ce607405f3
vina786 <vinay76b@gmail.com>
2024-12-07 05:54:50 +0530

    Add files via upload

---
+++ revnum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class revnum {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=5;j>=1;j--)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+            }
+        }
+
+    }
+
