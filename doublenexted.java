ecbf6d09b530a38c5068ef12ee6ffc111c000793
vina786 <vinay76b@gmail.com>
2024-12-07 05:51:18 +0530

    Add files via upload

---
+++ doublenexted.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class doublenexted {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+    for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=1;j++);
+    {
+        System.out.print("*");
+    }
+    System.out.println();
+}
+
+    for(int i=n-1;i>=1;i--)
+    {
+        for(int j>=1;j>1;j--)
+       {
+        System.out.println("*");
+       }
+       System.out.println();
+       } 
+    }
+
+    
+}
